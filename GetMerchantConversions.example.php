1ad813d67b811d60c7730aec1bbaf554695ba060
technicaladpia <50009828+technicaladpia@users.noreply.github.com>
2021-10-08 10:41:10 +0700

    Update GetMerchantConversions.example.php

--- GetMerchantConversions.example.php
+++ GetMerchantConversions.example.php
@@ -1,20 +1,42 @@
 <?php 
-	$token = base64_encode('merchant_id:password');
-	//Let add infor your account
-	$dataPost = array("sdate" => "20190530", "edate" => "20190530");
-	//add field to fillter exactly: status value(100:pending, 200:merchant confirm, 210:complete and payment, 300:cancel from merchant, 310:cancelled(Admin approved)), order_code, limit (limit field data respon), page (page pagination)
-	$curl = curl_init('http://event.adpia.vn/apiv2/getMerchantConversions');
-	// call api getConversions
-	curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
-	// method api
-	curl_setopt($curl, CURLOPT_POSTFIELDS, $dataPost);
-	// post api with data cause from input form
-	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-	// sent token to encoding base64 contain infor your account 
-	curl_setopt($curl, CURLOPT_HTTPHEADER, array(
-		'authorization:Basic '.$token)
-	);
-	$result = curl_exec($curl);
-	curl_close($curl);
-	//respon data 
-	var_dump($result);
+	header('Access-Control-Allow-Origin: *');
+        header('Content-type: application/json');
+
+        $curl = curl_init();
+
+        $author = base64_encode('username:password');
+
+        $data_post = array(
+            "sdate" => "20210101",
+            "edate" => "20210130",
+            "limit" => 10,
+            "page" => 3
+        );
+
+        curl_setopt_array($curl, array(
+          CURLOPT_URL => "https://api.adpia.vn/v2/merchant/get_conversions",
+          CURLOPT_RETURNTRANSFER => true,
+          CURLOPT_ENCODING => "",
+          CURLOPT_MAXREDIRS => 10,
+          CURLOPT_TIMEOUT => 30,
+          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+          CURLOPT_SSL_VERIFYPEER => FALSE,
+          CURLOPT_CUSTOMREQUEST => "POST",
+          CURLOPT_POSTFIELDS => json_encode($data_post),
+          CURLOPT_HTTPHEADER => array(
+            "authorization: Basic ".$author,
+            "cache-control: no-cache",
+            "content-type: application/json",
+          )
+        ));
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+        if ($err) {
+          echo "cURL Error #:" . $err;
+        } else {
+          echo $response;
+        }
